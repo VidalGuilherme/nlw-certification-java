f67d9a7e43f44c15cfb9a74928f917229835057c
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-02-11 10:34:02 -0300

    create questions entities

---
+++ src/main/java/com/vidal/nlw_certification/modules/questions/entities/QuestionEntity.java
@@ -0,0 +1,44 @@
+package com.vidal.nlw_certification.modules.questions.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.Builder;
+import org.hibernate.annotations.Collate;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "questions")
+public class QuestionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 50)
+    private String technology;
+
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "question_id")
+    private List<QuestionAlternativeEntity> alternatives;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
