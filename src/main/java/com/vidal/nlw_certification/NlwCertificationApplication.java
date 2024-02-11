3cc8fcc47c288bdaa27fcccf072f5532dadc5d87
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-02-11 10:15:26 -0300

    setup projetct

---
+++ src/main/java/com/vidal/nlw_certification/NlwCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.vidal.nlw_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwCertificationApplication.class, args);
+	}
+
+}
