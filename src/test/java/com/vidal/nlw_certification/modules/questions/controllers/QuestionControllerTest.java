7ab694727e692719aabf4c694379cbbf7335df1e
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-02-18 14:48:26 -0300

    questions controller tests

--- src/test/java/com/vidal/nlw_certification/modules/questions/controllers/QuestionControllerTest.java
+++ src/test/java/com/vidal/nlw_certification/modules/questions/controllers/QuestionControllerTest.java
@@ -1,5 +1,6 @@
 package com.vidal.nlw_certification.modules.questions.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.when;
@@ -19,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.*;
@@ -50,38 +52,39 @@ class QuestionControllerTest {
 
         // ACT + ASSERT
         mockMvc.perform(get("/questions/{id}", questionEntity.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(questionEntity.getId().toString()))
-                .andExpect(jsonPath("$.technology").value(questionEntity.getTechnology()))
-                .andExpect(jsonPath("$.description").value(questionEntity.getDescription()))
-                .andDo(print());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(questionEntity.getId().toString()))
+            .andExpect(jsonPath("$.technology").value(questionEntity.getTechnology()))
+            .andExpect(jsonPath("$.description").value(questionEntity.getDescription()))
+            .andDo(print());
+
     }
 
     @Test
-    void shouldCreateQuestion() throws Exception {
+    void shouldCreateQuestionWithStatus201() throws Exception {
         QuestionCreateDTO dto = new QuestionCreateDTO(
-                "Tests",
-                "Any Question?",
-                new ArrayList<>(Arrays.asList(
-                        new QuestionAlternativeCreateDTO("alternative 1", false),
-                        new QuestionAlternativeCreateDTO("alternative 2", false),
-                        new QuestionAlternativeCreateDTO("alternative 3", true),
-                        new QuestionAlternativeCreateDTO("alternative 4", false)
-                ))
+            "Tests",
+            "Any Question?",
+            new ArrayList<>(Arrays.asList(
+                new QuestionAlternativeCreateDTO("alternative 1", false),
+                new QuestionAlternativeCreateDTO("alternative 2", false),
+                new QuestionAlternativeCreateDTO("alternative 3", true),
+                new QuestionAlternativeCreateDTO("alternative 4", false)
+            ))
         );
         UUID id = UUID.fromString("c5f02721-6dc3-4fa6-b46d-6f2e8dca9c66");
         QuestionEntity questionEntity = new QuestionEntity(dto);
         questionEntity.setId(id);
 
-        when(questionRepository.save(questionEntity)).thenReturn(questionEntity);
+        when(questionRepository.save(any(QuestionEntity.class))).thenReturn(questionEntity);
 
         String json = objectMapper.writeValueAsString(dto);
-        mockMvc.perform(post("/questions")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                )
-                .andExpect(status().isCreated())
-                .andDo(print());
+        mockMvc.perform(
+            post("/questions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+            .andExpect(status().isCreated())
+            .andDo(print());
     }
 
     @Test
